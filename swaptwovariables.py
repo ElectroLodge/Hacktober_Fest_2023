2e9c0e86f073833d561a6c2dffe2fdb7e42d425e
ItzLowSky <116648794+ItzLowSky@users.noreply.github.com>
2022-10-25 22:52:17 +0530

    swap two variables python

---
+++ swaptwovariables.py
@@ -0,0 +1,10 @@
+P = int( input("Please enter value for P: "))  
+Q = int( input("Please enter value for Q: "))  
+   
+# To Swap the values of two variables using Addition and subtraction operator  
+P = P * Q    
+Q = P / Q   
+P = P / Q  
+   
+print ("The Value of P after swapping: ", P)  
+print ("The Value of Q after swapping: ", Q)  
