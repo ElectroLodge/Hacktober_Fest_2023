eb18a5ee894c64adc7696921fcc161a6891afbc9
DilaGit <146092681+DilaGit@users.noreply.github.com>
2023-10-02 21:45:15 +0530

    Added a simple calculator
    Added a simple calculator

---
+++ simple-cal.py
@@ -0,0 +1,44 @@
+#simple Claculator
+import time
+
+import discord
+from redbot.core import commands
+from TagScriptEngine import Interpreter, block
+
+
+class Calculator(commands.Cog):
+    """
+    Do math
+    """
+
+    __version__ = "1.0.0"
+
+    def __init__(self, bot):
+        self.bot = bot
+        blocks = [
+            block.MathBlock(),
+            block.RandomBlock(),
+            block.RangeBlock(),
+        ]
+        self.engine = Interpreter(blocks)
+
+    async def red_delete_data_for_user(self, **kwargs):
+        return
+
+    @commands.command(aliases=["calc"])
+    async def calculate(self, ctx, *, query):
+        """Math"""
+        query = query.replace(",", "")
+        engine_input = "{m:" + query + "}"
+        start = time.monotonic()
+        output = self.engine.process(engine_input)
+        end = time.monotonic()
+
+        output_string = output.body.replace("{m:", "").replace("}", "")
+        e = discord.Embed(
+            color=await ctx.embed_color(),
+            title=f"Input: `{query}`",
+            description=f"Output: `{output_string}`",
+        )
+        e.set_footer(text=f"Calculated in {round((end - start) * 1000, 3)} ms")
+        await ctx.send(embed=e)
