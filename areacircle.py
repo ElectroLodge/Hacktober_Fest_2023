433bafe7202c049a9338c1f2de23aa2c20a88380
DilaGit <146092681+DilaGit@users.noreply.github.com>
2023-10-02 06:54:13 +0530

    Find out the area of the circle
    Finding the area of the circle

--- areacircle.py
+++ areacircle.py
@@ -1,3 +1,5 @@
+#Area of the Circle
+#When the Radius is input you can have the Area as the output
 from math import pi
 r = float(input ("Input the radius of the circle : "))
 print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
