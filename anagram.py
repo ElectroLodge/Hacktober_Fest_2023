6144dae291bb082063ca7d6f967b7ab448395ce6
bhoomikarao22 <95897461+bhoomikarao22@users.noreply.github.com>
2023-10-03 20:06:50 +0530

    Update anagram.py
    This program takes input from user

--- anagram.py
+++ anagram.py
@@ -1,12 +1,12 @@
 def check(s1, s2):
      
     # the sorted strings are checked
-    if(sorted(s1)== sorted(s2)):
+    if(sorted(s1) == sorted(s2)):
         print("The strings are anagrams.")
     else:
         print("The strings aren't anagrams.")        
-         
+
 # driver code 
-s1 ="listen"
-s2 ="silent"
+s1 = input("Enter the first string: ")
+s2 = input("Enter the second string: ")
 check(s1, s2)
